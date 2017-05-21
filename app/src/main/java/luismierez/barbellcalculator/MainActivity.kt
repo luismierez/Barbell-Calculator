00c6179bd75da14a2ee68f6f517bb9f56a0fe1a8
Luis Mierez <luismierez@gmail.com>
2017-05-20 18:57:57 -0700

    Remove the plate buttons from the layout and the activity

--- app/src/main/java/luismierez/barbellcalculator/MainActivity.kt
+++ app/src/main/java/luismierez/barbellcalculator/MainActivity.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import com.jakewharton.rxbinding2.widget.RxTextView
 import io.reactivex.android.schedulers.AndroidSchedulers
 import kotlinx.android.synthetic.main.activity_main.*
-import luismierez.barbellcalculator.model.PlatePair
 
 class MainActivity : AppCompatActivity(), MainContract.View {
 
@@ -16,54 +15,6 @@ class MainActivity : AppCompatActivity(), MainContract.View {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        addHalf5.setOnClickListener {
-            presenter.addPlatePair(PlatePair(2.5))
-        }
-
-        add5.setOnClickListener {
-            presenter.addPlatePair(PlatePair(5.0))
-        }
-
-        add10.setOnClickListener {
-            presenter.addPlatePair(PlatePair(10.0))
-        }
-
-        add25.setOnClickListener {
-            presenter.addPlatePair(PlatePair(25.0))
-        }
-
-        add35.setOnClickListener {
-            presenter.addPlatePair(PlatePair(35.0))
-        }
-
-        add45.setOnClickListener {
-            presenter.addPlatePair(PlatePair(45.0))
-        }
-
-        removeHalf5.setOnClickListener {
-            presenter.removePlatePair(PlatePair(2.5))
-        }
-
-        remove5.setOnClickListener {
-            presenter.removePlatePair(PlatePair(5.0))
-        }
-
-        remove10.setOnClickListener {
-            presenter.removePlatePair(PlatePair(10.0))
-        }
-
-        remove25.setOnClickListener {
-            presenter.removePlatePair(PlatePair(25.0))
-        }
-
-        remove35.setOnClickListener {
-            presenter.removePlatePair(PlatePair(35.0))
-        }
-
-        remove45.setOnClickListener {
-            presenter.removePlatePair(PlatePair(45.0))
-        }
 
         presenter.registerWeight(RxTextView.afterTextChangeEvents(totalWeight)
                 .observeOn(AndroidSchedulers.mainThread())
